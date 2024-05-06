a7bb3b49031c705a4a360701042d02c06422ea0f
Harin Lee <lhr8440@gmail.com>
2024-05-06 21:44:59 +0900

    third commit

--- HW/Hw6_22300603_LeeHarin.cpp
+++ HW/Hw6_22300603_LeeHarin.cpp
@@ -5,31 +5,31 @@ using namespace std;
 
 class node {
 public:
-        string   name;
-        double      score;
-        node *link;
-        void   set_data(string   s, double n);
+    string   name;
+    double      score;
+    node *link;
+    void set_data(string   s, double n);
 };
 
-void   node::set_data(string s, double n) {
-           name = s;
-           score = n;
+void node::set_data(string s, double n) {
+    name = s;
+    score = n;
 }
 
 class my_list {
-     node   *head;
+    node   *head;
 public:
-        my_list() {
-                head = NULL;
-        }
-        void add_to_head(node   t);
-        void add_to_tail(node   t);
-        void delete_from_head();
-        void remove_a_node(string s);
-        double score_sum();
-        double get_score();
-        int  num_nodes();
-        void show_all_data();
+    my_list() {
+        head = NULL;
+    }
+    void add_to_head(node t);
+    void add_to_tail(node t);
+    void delete_from_head();
+    void remove_a_node(string s);
+    double score_sum();
+    double get_score();
+    int  num_nodes();
+    void show_all_data();
 };
 
 
@@ -39,42 +39,32 @@ int main()
 
     my_list a;
 
-    node   tmp;
+    node tmp;
 
     tmp.set_data("Kim", 83.5);
-
     a.add_to_head(tmp);
-
     tmp.set_data("Lee", 78.2);
-
     a.add_to_head(tmp);           // head 위치로 2개의 원소 추가
 
     cout << a.num_nodes() << " : " << a.score_sum() << "\n";  // 1단계 점검
 
     tmp.set_data("Park", 91.3);  
-
     a.add_to_tail(tmp);             // tail 위치로 1개의 원소 추가
-    
+
     cout << a.num_nodes() << " : " << a.score_sum() << "\n";  //2단계 점검
 
     tmp = a.delete_from_head();
-
     cout  << tmp.name << " is deleted.\n";   // 3단계 점검
 
     tmp.set_data("Choi", 85.1);  
-
     a.add_to_tail(tmp);             
-
     tmp.set_data("Ryu", 94.3);  
-
     a.add_to_head(tmp);             // 2개의 원소 추가
 
     cout << a.num_nodes()<< " : " << a.score_sum() << "\n";
-
     cout << "Park’s score : " << a.get_score("Park")<< "\n";  // 4단계 점검
 
     if (a.remove_a_node("Kim") == 1)
-
     cout <<  "Kim is deleted from the list. \n";     // 5단계 점검
 
     cout << a.num_nodes()<< " : " << a.score_sum() << "\n";  // 최종 점검       
